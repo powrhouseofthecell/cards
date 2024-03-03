3b79934a3f64125d0be958e5b9468b5813b83ba3
powrhouseofthecell <theshahzuhaib96@gmail.com>
2024-03-03 11:34:53 +0530

    init: basic setup

---
+++ deck.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+// Creating own type def
+type deck []string
+
+func new_deck() deck {
+
+	cards := []string{}
+
+	card_suit := []string{"Spades", "Dimonds", "Clubs", "Heart"}
+	card_value := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range card_suit {
+		for _, value := range card_value {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+
+}
+
+func (d deck) print() {
+	for _, card := range d {
+		fmt.Println(card)
+	}
+}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+
+	cards := new_deck()
+
+	cards.print()
+
+}
+
+func new_card() string {
+	return "Ace of Speads"
+}
